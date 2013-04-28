bf625f6b635af750059042fa7369425168bec5be
Wael Al-Sallami <wael.alsallami@gmail.com>
2013-04-28 06:11:00 -0500

    commented out some code

--- searchem.py
+++ searchem.py
@@ -20,26 +20,20 @@ def index():
 def search():
   return "this is the search page"
 
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-
-@app.teardown_request
-def close_db_connection(exception):
-    g.db.close()
+# @app.before_request
+# def before_request():
+#     g.db = connect_db()
 
 
-def connect_db():
-  return sqlite3.connect(app.config['DATABASE'])
+# @app.teardown_request
+# def close_db_connection(exception):
+#     g.db.close()
 
-
-def init_db():
-  with closing(connect_db()) as db:
-    with app.open_resource('schema.sql') as f:
-      db.cursor().executescript(f.read())
-    db.commit()
+# def init_db():
+#   with closing(connect_db()) as db:
+#     with app.open_resource('schema.sql') as f:
+#       db.cursor().executescript(f.read())
+#     db.commit()
 
 
 
