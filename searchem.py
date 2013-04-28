5aa14dd069db596561a0e19b62ae2e65d369f8a7
Wael Al-Sallami <wael.alsallami@gmail.com>
2013-04-28 06:15:30 -0500

    modified imports

--- searchem.py
+++ searchem.py
@@ -1,10 +1,7 @@
-from __future__ import with_statement
-from contextlib import closing
-from flask import Flask, render_template, url_for, g
+from flask import Flask, render_template, url_for, g #, request, session, redirect, abort, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
-import os
-# from flask import request, session, redirect, abort, flash
+
 
 app    = Flask(__name__)
 heroku = Heroku(app)
@@ -19,22 +16,6 @@ def index():
 @app.route('/search/')
 def search():
   return "this is the search page"
-
-# @app.before_request
-# def before_request():
-#     g.db = connect_db()
-
-
-# @app.teardown_request
-# def close_db_connection(exception):
-#     g.db.close()
-
-# def init_db():
-#   with closing(connect_db()) as db:
-#     with app.open_resource('schema.sql') as f:
-#       db.cursor().executescript(f.read())
-#     db.commit()
-
 
 
 if __name__ == '__main__':
